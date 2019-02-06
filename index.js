16c7e70f74e0a6318210b847276a1db5c9db0bc8
Nikolai Pawlowski <nikolaip@gmail.com>
2019-02-06 23:03:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Code your solution in this file!
 const returnFirstTwoDrivers = function (drivers) {
   return drivers.slice(0,2);
-}
+};
 
 const returnLastTwoDrivers = function (drivers) {
   return drivers.slice(drivers.length-2,drivers.lenght);
-}
+};
 
-const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
