ac6107680371542287cb0fbba05cb35b38cc390a
Nikolai Pawlowski <nikolaip@gmail.com>
2019-02-06 23:07:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,7 @@ const returnLastTwoDrivers = function (drivers) {
 };
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(multiplier) {
+
+}
