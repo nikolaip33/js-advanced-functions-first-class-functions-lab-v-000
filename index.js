3475473f12b59e80a137c23869efd85b835b31fa
Nikolai Pawlowski <nikolaip@gmail.com>
2019-02-06 22:56:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ const returnLastTwoDrivers = function (drivers) {
 }
 
 const selectingDrivers = function (drivers) {
-  console.log([returnFirstTwoDrivers(drivers), returnLastTwoDrivers(drivers)]}
+  console.log([returnFirstTwoDrivers(drivers), returnLastTwoDrivers(drivers)])
 }
